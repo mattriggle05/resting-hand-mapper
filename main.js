a8ae4d902feba07059f78ed0bd1993224577e4a4
mattriggle05 <mattriggle05@gmail.com>
2026-03-12 22:43:36 -0500

    Fix serving ? idk

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
 const canvas = document.getElementById('canvas');
-const activeTouches = new Map();
 let ctx;
+
 
 // MAKE THE CANVAS THE FULL PAGE
 
-function scaleCanvas() {
-    const dpr = window.devicePixelRatio || 1;
+function rescale() {
+    const ratio = window.devicePixelRatio || 1;
     const rect = canvas.getBoundingClientRect();
-    canvas.width = rect.width * dpr;
-    canvas.height = rect.height * dpr;
+    canvas.width = rect.width * ratio;
+    canvas.height = rect.height * ratio;
     ctx = canvas.getContext('2d');
-    ctx.scale(dpr, dpr);
+    ctx.scale(ratio, ratio);
 }
-
-scaleCanvas();
-window.addEventListener('resize', scaleCanvas);
+window.addEventListener('resize', rescale);
+rescale();
 
 
 
 // TOUCH STUFF
+
+const activeTouches = new Map();
 
 function draw() {
     const dpr = window.devicePixelRatio || 1;
     ctx.clearRect(0, 0, canvas.width / dpr, canvas.height / dpr);
     for (const touch of activeTouches.values()) {
         ctx.beginPath();
+        ctx.arc(touch.pageX, touch.pageY, 40, 0, 2 * Math.PI);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = `#000000`;
+        ctx.stroke();
         ctx.arc(touch.pageX, touch.pageY, 40, 0, 2 * Math.PI);
         ctx.lineWidth = 2;
         ctx.strokeStyle = `#00FF00`;
